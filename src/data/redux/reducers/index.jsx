42b5b171faa8dde8647768f765ea1d010e36fc74
priyanshitaneja <tanejapriyanshi1997@gmail.com>
2020-06-29 14:35:08 +0530

    reducers combined

--- src/data/redux/reducers/index.jsx
+++ src/data/redux/reducers/index.jsx
@@ -10,3 +10,75 @@
 //   // form: reduxFormReducer
 // });
 
+
+import * as actionTypes from "../../config/utils/constants";
+
+const initialState = {
+  photos: [],
+  loading: false,
+  error: false
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_PHOTOS_START:
+      return {
+        ...state,
+        loading: true
+      }
+
+    case actionTypes.FETCH_PHOTOS_SUCCESS:
+      return {
+        ...state,
+        photos: action.photos,
+        loading: false
+      }
+    case actionTypes.FETCH_PHOTOS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      }
+    case actionTypes.SEARCH_PHOTOS_START:
+      return {
+        ...state,
+        loading: true
+      }
+
+    case actionTypes.SEARCH_PHOTOS_SUCCESS:
+      return {
+        ...state,
+        photos: action.photos,
+        loading: false
+      }
+    case actionTypes.SEARCH_PHOTOS_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      }
+
+    case actionTypes.FETCH_MORE_PHOTOS_START:
+      return {
+        ...state,
+        loading: true
+      }
+
+    case actionTypes.FETCH_MORE_PHOTOS_SUCCESS:
+      return {
+        ...state,
+        photos: state.photos.concat(action.photos),
+        loading: false
+      }
+    case actionTypes.FETCH_MORE_PHOTOS_FAIL:
+      return {
+        ...state,
+        error: action.error,
+        loading: false
+      }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
