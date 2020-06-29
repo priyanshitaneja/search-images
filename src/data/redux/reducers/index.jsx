5708433cdeaed438a2dd7141cf112a144008ee2d
priyanshitaneja <tanejapriyanshi1997@gmail.com>
2020-06-29 14:27:13 +0530

    reducers updated

--- src/data/redux/reducers/index.jsx
+++ src/data/redux/reducers/index.jsx
@@ -1,11 +1,12 @@
-import { combineReducers } from 'redux';
-// // import { reducer as reduxFormReducer } from 'redux-form';
-// import { searchPhotos } from './searchPhotos';
-// import { selectedPhoto } from './selectedPhoto';
+// import { combineReducers } from 'redux';
+// // // import { reducer as reduxFormReducer } from 'redux-form';
+// // import { searchPhotos } from './searchPhotos';
+// // import { selectedPhoto } from './selectedPhoto';
 
-export default combineReducers({
-  xyz: () => "hi"
-  // photos: searchPhotos
-  // selectedPhoto: selectedPhoto,
-  // form: reduxFormReducer
-});
+// export default combineReducers({
+//   xyz: () => "hi"
+//   // photos: searchPhotos
+//   // selectedPhoto: selectedPhoto,
+//   // form: reduxFormReducer
+// });
+
